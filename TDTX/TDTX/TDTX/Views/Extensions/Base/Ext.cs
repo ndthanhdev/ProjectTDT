94e901430e3abe420fe1bb796c074efe9bdaff52
thanhgola <thanhgola@gmail.com>
2016-06-30 02:29:25 +0700

    faild

---
+++ TDTX/TDTX/TDTX/Views/Extensions/Base/Ext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace TDTX.Views.Extensions.Base
+{
+    public static class Ext
+    {
+        public static PropertyInfo GetPropertyEx(this Type type, string name)
+        {
+            while (type != null)
+            {
+                var typeInfo = type.GetTypeInfo();
+                var p = typeInfo.GetDeclaredProperty(name);
+                if (p != null)
+                    return p;
+                type = typeInfo.BaseType;
+            }
+            return null;
+        }
+    }
+}
--- TDTX/TDTX/TDTX/Views/Extensions/TranslateBind.cs
+++ TDTX/TDTX/TDTX/Views/Extensions/TranslateBind.cs
@@ -1,37 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TDTX.Views.Extensions.Base;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TDTX.Views.Extensions
 {
-    public class TranslateBind : IMarkupExtension
-    {
-        public Binding Text { get; set; }
+    // refer wpf http://stackoverflow.com/questions/10328802/markupextension-with-binding-parameters
+    // refer https://forums.xamarin.com/discussion/36884/missing-implementation-of-iprovidevaluetarget-targetproperty-property-imarkupextension
+    //public class TranslateBind : IMarkupExtension
+    //{
+    //    public Binding Text { get; set; }
 
-        public object ProvideValue(IServiceProvider serviceProvider)
-        {
-            IProvideValueTarget target = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
-            BindableObject targetObject;
-            BindableProperty targetProperty;
-            if (target != null && target.TargetObject is BindableObject && target.TargetProperty is BindableProperty)
-            {
-                targetObject = (BindableObject)target.TargetObject;
-                targetProperty = (BindableProperty)target.TargetProperty;
-            }
-            else
-            {
-                return this;
-            }
-            targetObject.SetBinding(targetProperty,Text.Path);
-            var text = targetObject.GetValue(targetProperty);
-            return text;
-        }
-        private static BindableProperty TextBindingSinkProperty = BindableProperty.CreateAttached(
-            propertyName: "TextBindingSinkProperty",
-            returnType: typeof(object),// set the desired type of Param1 for at least runtime type safety check
-            declaringType: typeof(TranslateBind),
-            defaultValue: null);
-    }
+    //    public object ProvideValue(IServiceProvider serviceProvider)
+    //    {
+            
+    //        IProvideValueTarget target = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
+    //        BindableObject targetObject;
+
+
+    //        //BindableProperty targetProperty;
+    //        var targetProperty = target.GetType().GetPropertyEx("Text");
+    //        targetProperty.
+    //        if (target != null && target.TargetObject is BindableObject && target.TargetProperty is BindableProperty)
+    //        {
+    //            targetObject = (BindableObject)target.TargetObject;
+    //            targetProperty = (BindableProperty)target.TargetProperty;
+    //        }
+    //        else
+    //        {
+    //            return this;
+    //        }
+    //        targetObject.SetBinding(targetProperty,Text.Path);
+    //        var text = targetObject.GetValue(targetProperty);
+    //        return text;
+    //    }
+    //    private static BindableProperty TextBindingSinkProperty = BindableProperty.CreateAttached(
+    //        propertyName: "TextBindingSinkProperty",
+    //        returnType: typeof(object),// set the desired type of Param1 for at least runtime type safety check
+    //        declaringType: typeof(TranslateBind),
+    //        defaultValue: null);
+    //}
 }
