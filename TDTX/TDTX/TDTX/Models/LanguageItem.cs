270be70ffdc0185b4b2a3988b128d06307d36ad2
thanhgola <thanhgola@gmail.com>
2016-07-04 21:37:49 +0700

    option

---
+++ TDTX/TDTX/TDTX/Models/LanguageItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDTX.Models
+{
+    public class LanguageItem
+    {
+        public string CultureName { get; set; }
+        public string IconPath { get; set; }
+    }
+}
---
+++ TDTX/TDTX/TDTX/ViewModels/OptionPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using GalaSoft.MvvmLight;
+using TDTX.Models;
+
+namespace TDTX.ViewModels
+{
+    class OptionPageViewModel : ViewModelBase
+    {
+        private static OptionPageViewModel _instance;
+        public static OptionPageViewModel Instance => _instance ?? new OptionPageViewModel();
+        private ObservableCollection<LanguageItem> _languageItemsitems;
+        public ObservableCollection<LanguageItem> LanguageItems
+        {
+            get { return _languageItemsitems = _languageItemsitems ?? new ObservableCollection<LanguageItem>(); }
+            set { Set(ref _languageItemsitems, value); }
+        }
+
+        private OptionPageViewModel()
+        {
+            _languageItemsitems= new ObservableCollection<LanguageItem>()
+            {
+                new LanguageItem() {CultureName = "en",IconPath = ""},
+                new LanguageItem() {CultureName = "lo",IconPath = ""},
+                new LanguageItem() {CultureName = "vi",IconPath = ""}
+            };
+
+        }
+    }
+}
