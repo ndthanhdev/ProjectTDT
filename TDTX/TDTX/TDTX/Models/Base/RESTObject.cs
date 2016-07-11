4be6f56520fb1d050610d750f8c4ea686520c3b1
thanhgola <thanhgola@gmail.com>
2016-07-12 00:36:52 +0700

    create RESTObject

---
+++ TDTX/TDTX/TDTX/Models/Base/RESTObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace TDTX.Models.Base
+{
+    public abstract class RESTObject
+    {
+        [JsonIgnore]
+        public abstract string[] Attributes { get; }
+
+        public virtual string GetQuery(params string[] parameters)
+        {
+            if (Attributes.Length != parameters.Length)
+                return null;
+            List<string> list = new List<string>();
+            for (int i = 0; i < Attributes.Length; i++)
+                list.Add(string.Concat(Attributes[i], "=", parameters[i]));
+            return "?" + string.Join("&", list);
+        }
+    }
+}
