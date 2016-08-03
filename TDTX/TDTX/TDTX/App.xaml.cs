e5526e98de3c0d493dd3ea0f70ab919b415ecabb
thanhgola <thanhgola@gmail.com>
2016-08-03 22:58:40 +0700

    save

--- TDTX/TDTX/TDTX/App.xaml.cs
+++ TDTX/TDTX/TDTX/App.xaml.cs
@@ -25,12 +25,13 @@ namespace TDTX
         }
 
         private bool _loaded = false;
+
         protected override async void OnStart()
         {
             await Task.Yield();
-            
+
             // Handle when your app starts  
-            await Task.WhenAll(Settings.Instance.Load<Settings>(),TimeTable.Instance.Load<TimeTable>());
+            await Task.WhenAll(Settings.Instance.Load<Settings>(), TimeTable.Instance.Load<TimeTable>());
             _loaded = true;
             if (Settings.Instance.CanTryLogin())
                 MainPage = new MainPage();
@@ -41,11 +42,11 @@ namespace TDTX
         protected override async void OnSleep()
         {
             //await Task.WhenAll(Settings.Instance.Save(), TimeTable.Instance.Save());
-            
+
             await Task.Yield();
             if (_loaded)
             {
-                await Task.WhenAll(Settings.Instance.Save(),TimeTable.Instance.Save());
+                await Task.WhenAll(Settings.Instance.Save(), TimeTable.Instance.Save());
             }
             await this.SavePropertiesAsync();
         }
@@ -53,9 +54,10 @@ namespace TDTX
         protected override async void OnResume()
         {
             // Handle when your app resumes
-            //await Settings.Instance.Load<Settings>();
-            //await TimeTable.Instance.Load<TimeTable>();
-
+            if (_loaded)
+            {
+                await Task.WhenAll(Settings.Instance.Load<Settings>(), TimeTable.Instance.Load<TimeTable>());
+            }
         }
     }
 }
