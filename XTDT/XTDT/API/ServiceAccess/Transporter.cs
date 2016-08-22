778713be27ac7b045b75ca5009b4116bad224874
thanhgola <thanhgola@gmail.com>
2016-08-22 21:33:50 +0700

    design

--- XTDT/XTDT/API/ServiceAccess/Transporter.cs
+++ XTDT/XTDT/API/ServiceAccess/Transporter.cs
@@ -3,6 +3,8 @@ using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -14,7 +16,7 @@ namespace XTDT.API.ServiceAccess
 {
     public static class Transporter
     {
-        public static string Host = "http://trautre.azurewebsites.net/api.php?";
+        public static string Host = "http://api.trautre.cf/api.php?";
         /// <summary>
         /// 
         /// </summary>
@@ -80,16 +82,35 @@ namespace XTDT.API.ServiceAccess
         {
             await Task.Yield();
 
-            using (HttpClient client = new HttpClient())
+            using (HttpClientHandler handler = new HttpClientHandler())
             {
-
-                client.Timeout = TimeSpan.FromSeconds(60);
-                //var response = await client.GetAsync(Host + query);
-                //var bytes = await response.Content.ReadAsByteArrayAsync();
-
-                var result = await client.GetStringAsync(Host + query);//Encoding.UTF8.GetString(bytes);
-                return result;
+                handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                using (HttpClient client = new HttpClient(handler))
+                {
+                    client.Timeout = TimeSpan.FromSeconds(60);
+                    var result = await client.GetStringAsync(Host + query);//Encoding.UTF8.GetString(bytes);
+                    return result;
+                }
             }
+            
+            //HttpClient client = new HttpClient(handler);
+            //client.DefaultRequestHeaders.Accept.ParseAdd("text/html, application/xhtml+xml, application/json, image/jxr, */*");
+            //client.DefaultRequestHeaders.AcceptEncoding.ParseAdd("gzip, deflate");
+            //client.DefaultRequestHeaders.AcceptLanguage.ParseAdd("en-US, en; q=0.7, vi; q=0.3");
+            //client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393");
+            //var url = Host + query;
+            //var r2 = await client.GetStringAsync(url);
+            //var respond = await client.GetAsync(url);
+            //var stream = await respond.Content.ReadAsStreamAsync();
+            //using (var reader = new StreamReader(stream, Encoding.UTF8))
+            //{
+            //    string l = reader.ReadToEnd();
+
+            //}
+
+
+            //var result = Encoding.UTF8.GetString(await respond.Content.ReadAsByteArrayAsync());
+            //return result;
         }
 
     }
