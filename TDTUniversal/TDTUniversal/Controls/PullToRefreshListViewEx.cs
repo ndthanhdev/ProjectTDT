707a2468e30ea09a38647380e43cb3323baeb110
thanh <thanhgola@gmail.com>
2016-09-24 03:01:52 +0700

    201609240300 big step

---
+++ TDTUniversal/TDTUniversal/Controls/PullToRefreshListViewEx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace TDTUniversal.Controls
+{
+    [TemplatePart(Name = PartScroller, Type = typeof(ScrollViewer))]
+    public sealed class PullToRefreshListViewEx : Microsoft.Toolkit.Uwp.UI.Controls.PullToRefreshListView
+    {
+
+        public ICommand LoadMoreCommand
+        {
+            get { return (ICommand)GetValue(LoadMoreCommandProperty); }
+            set { SetValue(LoadMoreCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LoadMoreCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LoadMoreCommandProperty =
+            DependencyProperty.Register("LoadMoreCommand", typeof(ICommand), typeof(PullToRefreshListViewEx), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Occurs when the user has requested content to be refreshed
+        /// </summary>
+        public event EventHandler LoadMoreRequested;
+
+        private const string PartScroller = "ScrollViewer";
+
+        private ScrollViewer _scroller;
+
+        public PullToRefreshListViewEx()
+        {
+            this.DefaultStyleKey = typeof(PullToRefreshListViewEx);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            _scroller = GetTemplateChild(PartScroller) as ScrollViewer;
+            if (_scroller != null)
+            {
+                _scroller.ViewChanged += _scroller_ViewChanged;
+            }
+            base.OnApplyTemplate();
+        }
+
+        private void _scroller_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            if (PullThreshold > _scroller.ScrollableHeight - _scroller.VerticalOffset)
+            {
+                LoadMoreRequested?.Invoke(this, new EventArgs());
+                if (LoadMoreCommand != null && LoadMoreCommand.CanExecute(null))
+                {
+                    LoadMoreCommand.Execute(null);
+                }
+            }
+        }
+    }
+}
--- TDTUniversal/TDTUniversal/ViewModels/NotificationPageViewModel.cs
+++ TDTUniversal/TDTUniversal/ViewModels/NotificationPageViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using TDTUniversal.API;
 using TDTUniversal.API.Requests;
 using TDTUniversal.API.Respond;
@@ -16,8 +17,9 @@ namespace TDTUniversal.ViewModels
 {
     public class NotificationPageViewModel : ViewModelBase
     {
-        private const int _numberOfRequest = 3;
-        private const int _maximunPageSave = 10;
+        private int _currentPage = 0;
+        private const int _threshold = 10;
+        private bool _isRunning = false;
 
         ObservableCollection<ThongBao> _thongBaos;
         public ObservableCollection<ThongBao> ThongBaos
@@ -31,6 +33,12 @@ namespace TDTUniversal.ViewModels
             await LoadData();
             await UpdateData();
         }
+
+        public ICommand LoadMoreCommand => new DelegateCommand(async () =>
+         {
+             await GetNoti();
+         }, () => !_isRunning);
+
         public async Task LoadData()
         {
             await Task.Yield();
@@ -39,29 +47,34 @@ namespace TDTUniversal.ViewModels
         public async Task UpdateData()
         {
             await Task.Yield();
-            var first = await ApiClient.GetAsync<DSThongBaoRequest, DSThongBao>(new DSThongBaoRequest(LocalDataService.Instance.StudentID),
+            await GetNoti();
+        }
+
+        public async Task GetNoti()
+        {
+            _isRunning = true;
+            await Task.Yield();
+            _currentPage++;
+            var package = await ApiClient.GetAsync<DSThongBaoRequest, DSThongBao>(new DSThongBaoRequest(LocalDataService.Instance.StudentID, _currentPage),
                 TokenService.GetTokenProvider());
-            if (!first.Status)
+            if (!package.Status)
                 return;
             using (TDTContext db = new TDTContext())
             {
-                db.ThongBao.RemoveRange(from tb in db.ThongBao select tb);
-                db.ThongBao.AddRange(from tbr in first.Respond.Thongbao select new ThongBao(tbr));
-                ThongBaos = new ObservableCollection<ThongBao>(from tb in db.ThongBao select tb);
+                if (_currentPage == 1)
+                {
+                    db.ThongBao.RemoveRange(from tb in db.ThongBao select tb);
+                }
+                var newtblist = from tbr in package.Respond.Thongbao select new ThongBao(tbr);
+                foreach (ThongBao tb in newtblist)
+                {
+                    ThongBaos.Add(tb);
+                }
+                db.AddRange(newtblist);
+                await db.SaveChangesAsync();
             }
-            //List<Task<Package<DSThongBaoRequest, DSThongBao>>> quests = new List<Task<Package<DSThongBaoRequest, DSThongBao>>>();
-            //var range = Math.Min(_maximunPageSave, first.Respond.Numpage);
-            //for (int i = 2; i <= range; i += _numberOfRequest)
-            //{
-            //    for (int j = i; j <= Math.Min(i + _numberOfRequest, range); j++)
-            //    {
-            //        var t = ApiClient.GetAsync<DSThongBaoRequest, DSThongBao>(new DSThongBaoRequest(LocalDataService.Instance.StudentID, i),
-            //        TokenService.GetTokenProvider());
-            //        quests.Add(t);
-            //    }
-            //    await Task.WhenAll(quests);
-            //}
-            var newData = new List<ThongBao>();
+            _isRunning = false;
+
         }
     }
 }
